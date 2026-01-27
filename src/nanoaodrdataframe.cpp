6cbd033059be43d96c66aba696431eaf9c58799b
Manasa Sahoo <msahoo@cmslpc305.fnal.gov>
2026-01-26 22:32:01 -0600

    this is the 2024 set up with out correction applied

--- src/nanoaodrdataframe.cpp
+++ src/nanoaodrdataframe.cpp
@@ -20,13 +20,13 @@ using namespace ROOT;
 int main(void) {
 
 	TChain c1("Events");
-	c1.Add("/uscms/home/msahoo/nobackup/Project_tzq/EGamma2022_EraF_v15.root"); //data
+//	c1.Add("/uscms/home/msahoo/nobackup/Project_tzq/EGamma2022_EraF_v15.root"); //data
 //	c1.Add("/uscms/home/msahoo/nobackup/Project_tzq/2022Data_Muon.root"); //data
 //	c1.Add("/uscms/home/msahoo/nobackup/Project_tzq/2022_postEE_tZq.root"); // MC
-	//c1.Add("root://cmsxrootd.fnal.gov//store/data/Run2022D/Muon/NANOAOD/16Dec2023-v1/50000/fa77d341-cad2-4902-a837-308655dbca47.root");
+	c1.Add("root://cmsxrootd.fnal.gov//store/mc/RunIII2024Summer24NanoAODv15/TZQB-Zto2L-4FS_Bin-MLL-30_TuneCP5_13p6TeV_amcatnlo-pythia8/NANOAODSIM/Madgraph_2_6_5_150X_mcRun3_2024_realistic_v2-v2/2810000/049a96b9-df0c-4e43-ba77-ec1728657c95.root");
 	BaseAnalyser nanoaodrdf(&c1, "top_tzq.root");
-	nanoaodrdf.setParams(2022, "PreEE", -1);
-//        nanoaodrdf.setParams(2022, "PostEE", -1);
+	//nanoaodrdf.setParams(2022, "PreEE", -1);
+        nanoaodrdf.setParams(2022, "PostEE", -1);
 	nanoaodrdf.setHLT();
 
 	string goodjsonfname = "data/GoldenJSON/golden_json_latest_2022.json";
